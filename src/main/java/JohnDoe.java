f5b6d5069390bc9d36bb9fbbba2429ac9364858a
CH144 <177772377+CH144@users.noreply.github.com>
2026-01-16 13:43:39 +0000

    Rename Duke.java to JohnDoe.java

---
+++ src/main/java/JohnDoe.java
@@ -0,0 +1,7 @@
+public class JohnDoe {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm JohnDoe");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
