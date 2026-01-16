0be1d90eeb88a5d5e5a9a340b6e564d8d151a884
CH144 <177772377+CH144@users.noreply.github.com>
2026-01-16 17:35:39 +0000

    Add Deadline.java

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,15 @@
+class Deadline extends Task {
+    private String deadline;
+
+    Deadline(String taskName, String deadline) {
+        super(taskName);
+        this.deadline = deadline;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[D]%s (by: %s)",
+                super.toString(),
+                deadline);
+    }
+}
