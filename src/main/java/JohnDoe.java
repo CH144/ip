f80f6b5c29fd78e4ee6aea8d1b9fd394f0e46c26
CH144 <177772377+CH144@users.noreply.github.com>
2026-01-16 14:22:41 +0000

    Add Echo feature to JohnDoe.java

--- src/main/java/JohnDoe.java
+++ src/main/java/JohnDoe.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class JohnDoe {
     public static void main(String[] args) {
         System.out.println("Hello! I'm JohnDoe");
         System.out.println("What can I do for you?");
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String userRequest = scanner.nextLine();
+
+            if (userRequest.equalsIgnoreCase("bye")) {
+                scanner.close();
+                break;
+            }
+
+            System.out.println(userRequest);
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
