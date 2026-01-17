c9baa84e01f81d1d407509be716ec7a089df72ab
CH144 <177772377+CH144@users.noreply.github.com>
2026-01-17 18:49:45 +0000

    Move parsing implementation to Parser.java

---
+++ src/main/java/Parser.java
@@ -0,0 +1,64 @@
+class Parser {
+    private Parser() {
+        // intentionally blank
+    }
+
+    public static boolean parse(String userInput, Tasklist tasklist) {
+        String[] tokens = userInput.split("\\s+", 2);
+        String command = tokens[0];
+
+        if (tokens.length > 1) {
+            switch (command) {
+            case "todo":
+                tasklist.addTodo(tokens[1]);
+                break;
+            case "deadline":
+                tasklist.addDeadline(tokens[1]);
+                break;
+            case "event":
+                tasklist.addEvent(tokens[1]);
+                break;
+            case "mark":
+                tasklist.markTask(tokens[1]);
+                break;
+            case "unmark":
+                tasklist.unmarkTask(tokens[1]);
+                break;
+            default:
+                Ui.printUnknownCommandHelp();
+                break;
+            }
+        } else {
+            switch (command) {
+            case "bye":
+                Ui.printFarewell();
+                return false;
+            case "help":
+                Ui.printCommandsListHelp();
+                break;
+            case "list":
+                tasklist.printTasks();
+                break;
+            case "todo":
+                Ui.printTodoHelp();
+                break;
+            case "deadline":
+                Ui.printDeadlineHelp();
+                break;
+            case "event":
+                Ui.printEventHelp();
+                break;
+            case "mark":
+                Ui.printMarkHelp();
+                break;
+            case "unmark":
+                Ui.printUnmarkHelp();
+                break;
+            default:
+                Ui.printUnknownCommandHelp();
+                break;
+            }
+        }
+        return true;
+    }
+}
