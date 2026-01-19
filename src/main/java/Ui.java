dd2b638435a1331b325c877f3ac759f6a983817c
CH144 <177772377+CH144@users.noreply.github.com>
2026-01-19 01:24:06 +0000

    Move scanner implementation to Ui.java

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,23 +1,32 @@
+import java.util.Scanner;
+
 class Ui {
-    private Ui() {
-        // intentionally blank
+    private Scanner scanner;
+
+    public Ui() {
+        scanner = new Scanner(System.in);
     }
 
-    public static void printGreeting() {
+    public String readUserInput() {
+        return scanner.nextLine().strip();
+    }
+
+    public void printGreeting() {
         System.out.printf("  Hello! I'm JohnDoe.\n"
                 + "  What can I do for you?\n"
                 + "  Enter 'help' for a list of available commands.\n\n> ");
     }
 
-    public static void printFarewell() {
+    public void printFarewell() {
+        scanner.close();
         System.out.printf("  Bye. Hope to see you again soon!\n");
     }
 
-    public static void printUnknownCommandHelp() {
+    public void printUnknownCommandHelp() {
         System.out.printf("  Enter 'help' for a list of all available commands.\n\n> ");
     }
 
-    public static void printCommandsListHelp() {
+    public void printHelp() {
         System.out.printf("  Available commands:\n"
                 + "    list\n"
                 + "    todo\n"
@@ -31,39 +40,39 @@ class Ui {
                 + "  Commands are case-sensitive.\n\n> ");
     }
 
-    public static void printTodoHelp() {
+    public void printTodoHelp() {
         System.out.printf("  info    : add a task.\n"
                 + "  usage   : todo TASK_NAME\n"
                 + "  example : todo watch lecture recording\n\n> ");
     }
 
-    public static void printDeadlineHelp() {
+    public void printDeadlineHelp() {
         System.out.printf("  info    : add a task with a deadline.\n"
                 + "  usage   : deadline TASK_NAME /by DEADLINE\n"
                 + "  example : deadline week 3 iP /by 30 Jan\n\n> ");
     }
 
-    public static void printEventHelp() {
+    public void printEventHelp() {
         System.out.printf("  info    : add a task with a start and end.\n"
                 + "  usage   : event TASK_NAME /from START_TIME /to END_TIME\n"
                 + "  example : event tP meeting /from Mon 10am /to 12nn\n\n> ");
     }
 
-    public static void printDeleteHelp() {
+    public void printDeleteHelp() {
         System.out.printf("  info    : removes an existing task.\n"
                 + "  usage   : delete TASK_NUMBER\n"
                 + "  example : delete 1\n"
                 + "  Use 'list' to view all tasks and their corresponding number.\n\n> ");
     }
 
-    public static void printMarkHelp() {
+    public void printMarkHelp() {
         System.out.printf("  info    : set a task as done, indicated by '[X]'.\n"
                 + "  usage   : mark TASK_NUMBER\n"
                 + "  example : mark 1\n"
                 + "  Use 'list' to view all tasks and their corresponding number.\n\n> ");
     }
 
-    public static void printUnmarkHelp() {
+    public void printUnmarkHelp() {
         System.out.printf("  info    : set a task as not done, indicated by '[ ]'.\n"
                 + "  usage   : unmark TASK_NUMBER\n"
                 + "  example : unmark 1\n"
