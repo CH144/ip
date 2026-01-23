088855eec27ba606bfd4a8ac802ded042055aee6
CH144 <177772377+CH144@users.noreply.github.com>
2026-01-23 22:35:43 +0000

    Update to use JohnDoeException

--- src/main/java/DeadlineCommand.java
+++ src/main/java/DeadlineCommand.java
@@ -11,30 +11,30 @@ class DeadlineCommand extends Command {
         opTask = Optional.empty();
     }
 
-    public DeadlineCommand(String input) {
+    public DeadlineCommand(String input) throws JohnDoeException {
         super(false);
 
         String[] tokens = input.split("\\s+");
 
         for (String token : tokens) {
             if (token != null && !token.equals("/by") && token.contains("/by")) {
-                throw new IllegalArgumentException(
+                throw new JohnDoeException(
                         "  There should be spaces before and after '/by'.\n" + HELP_SUFFIX);
             }
         }
 
         if (Collections.frequency(Arrays.asList(tokens), "/by") != 1) {
-            throw new IllegalArgumentException("  Exactly one '/by' must be used.\n" + HELP_SUFFIX);
+            throw new JohnDoeException("  Exactly one '/by' must be used.\n" + HELP_SUFFIX);
         }
 
         int byIndex = Arrays.asList(tokens).indexOf("/by");
 
         if (byIndex == 0) {
-            throw new IllegalArgumentException("  Please provide a task name.\n" + HELP_SUFFIX);
+            throw new JohnDoeException("  Please provide a task name.\n" + HELP_SUFFIX);
         }
 
         if (byIndex == tokens.length - 1) {
-            throw new IllegalArgumentException("  Please provide a deadline.\n" + HELP_SUFFIX);
+            throw new JohnDoeException("  Please provide a deadline.\n" + HELP_SUFFIX);
         }
 
         String taskName = String.join(" ", Arrays.copyOfRange(tokens, 0, byIndex));
--- src/main/java/MarkCommand.java
+++ src/main/java/MarkCommand.java
@@ -8,12 +8,17 @@ class MarkCommand extends Command {
         opInt = OptionalInt.empty();
     }
 
-    public MarkCommand(String input) {
+    public MarkCommand(String input) throws JohnDoeException {
         super(false);
-        opInt = OptionalInt.of(Integer.parseInt(input) - 1);
+        try {
+            opInt = OptionalInt.of(Integer.parseInt(input) - 1);
+        } catch (NumberFormatException e) {
+            throw new JohnDoeException("  Task number does not exist.\n"
+                    + "  Enter 'list' to view all tasks and their corresponding number.\n\n> ");
+        }
     }
 
-    public void run(TaskList taskList, Ui ui) {
+    public void run(TaskList taskList, Ui ui) throws JohnDoeException {
         if (opInt.isPresent()) {
             taskList.markTask(opInt.getAsInt(), ui);
         } else {
