2ae1c71a087ca8ee18a4637ab3a17090a91a0e43
CH144 <177772377+CH144@users.noreply.github.com>
2026-01-30 22:51:32 +0000

    Change implementation to fxml

--- src/main/java/johndoe/app/Main.java
+++ src/main/java/johndoe/app/Main.java
@@ -1,95 +1,33 @@
 package johndoe.app;
 
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import johndoe.gui.DialogBox;
+import johndoe.gui.MainWindow;
 
+/**
+ * GUI for the {@code JohnDoe} application.
+ */
 public class Main extends Application {
-    private static final String FILE_PATH = "./data/tasks.txt";
-
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-    private JohnDoe johnDoe;
+    private JohnDoe johnDoe = new JohnDoe("./data/tasks.txt");
 
     @Override
     public void start(Stage stage) {
-        johnDoe = new JohnDoe(FILE_PATH);
-
-        stage.setTitle("JohnDoe");
-        stage.setResizable(false);
-        stage.setMinHeight(800.0);
-        stage.setMinWidth(600.0);
-
-        dialogContainer = new VBox();
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-        loadTasks();
-
-        scrollPane = new ScrollPane();
-        scrollPane.setPrefSize(600, 770);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-        scrollPane.setContent(dialogContainer);
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        userInput = new TextField();
-        userInput.setOnAction((event) -> {
-            handleUserInput();
-        });
-        userInput.setPrefWidth(525.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-        AnchorPane.setLeftAnchor(userInput, 1.0);
-
-        sendButton = new Button("Send");
-        sendButton.setOnMouseClicked((event) -> {
-            handleUserInput();
-        });
-        sendButton.setPrefWidth(55.0);
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-        mainLayout.setPrefSize(600.0, 800.0);
-
-        scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    private void loadTasks() {
-        String output = johnDoe.loadTasks();
-        if (!output.equals("")) {
-            dialogContainer.getChildren().addAll(
-                    DialogBox.getJohnDoeBox(output)
-            );
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            stage.setResizable(false);
+            fxmlLoader.<MainWindow>getController().setJohnDoe(johnDoe);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    }
-
-    private void handleUserInput() {
-        String input = userInput.getText();
-        String output = johnDoe.processUserInput(input);
-        if (output.equals("bye")) {
-            Platform.exit();
-        }
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserBox(input),
-                DialogBox.getJohnDoeBox(output)
-        );
-        userInput.clear();
     }
 }
--- src/main/java/johndoe/gui/DialogBox.java
+++ src/main/java/johndoe/gui/DialogBox.java
@@ -1,24 +1,48 @@
 package johndoe.gui;
 
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 
+/**
+ * Encapsulates the user input and {@code JohnDoe} output to be displayed.
+ */
 public class DialogBox extends HBox {
-    private Label text;
+    @FXML
+    private Label dialog;
 
-    private DialogBox(String s) {
-        text = new Label(s);
-        text.setWrapText(true);
-        this.getChildren().addAll(text);
+    private DialogBox(String text) {
+        try {
+            FXMLLoader fxmlLoader =
+                    new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
+            fxmlLoader.setController(this);
+            fxmlLoader.setRoot(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        dialog.setText(text);
     }
 
+    /**
+     * Returns a new {@code DialogBox}.
+     * Aligned right for the user.
+     */
     public static DialogBox getUserBox(String s) {
         DialogBox d = new DialogBox(s);
         d.setAlignment(Pos.TOP_RIGHT);
         return d;
     }
 
+    /**
+     * Returns a new {@code DialogBox}.
+     * Aligned left for {@code JohnDoe}.
+     */
     public static DialogBox getJohnDoeBox(String s) {
         DialogBox d = new DialogBox(s);
         d.setAlignment(Pos.TOP_LEFT);
