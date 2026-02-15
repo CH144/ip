c825fbbde2e1ed46f6f3cd8be0f40347eceb9795
CH144 <177772377+CH144@users.noreply.github.com>
2026-02-15 21:54:07 +0000

    Add more test cases

--- src/test/java/johndoe/command/DeadlineCommandTest.java
+++ src/test/java/johndoe/command/DeadlineCommandTest.java
@@ -7,13 +7,59 @@ import org.junit.jupiter.api.Test;
 import johndoe.exception.JohnDoeException;
 
 public class DeadlineCommandTest {
+    private static final String HELP_SUFFIX = "  Enter 'deadline' for more help.\n\n";
+
+    @Test
+    public void constructDeadline_byKeywordIsSubstring_exceptionThrown() {
+        try {
+            new DeadlineCommand("sleep/by 01/01/2026 0030");
+        } catch (JohnDoeException e) {
+            String errorMessage =
+                    "  There should be spaces before and after '/by'.\n" + HELP_SUFFIX;
+            assertEquals(errorMessage, e.getMessage());
+        }
+    }
+
+    @Test
+    public void constructDeadline_byKeywordRepeated_exceptionThrown() {
+        try {
+            new DeadlineCommand("sleep /by /by 01/01/2026 0030");
+        } catch (JohnDoeException e) {
+            String errorMessage =
+                    "  Exactly one '/by' must be used.\n" + HELP_SUFFIX;
+            assertEquals(errorMessage, e.getMessage());
+        }
+    }
+
     @Test
     public void constructDeadline_noName_exceptionThrown() {
         try {
-            new DeadlineCommand("/by 1pm");
+            new DeadlineCommand("/by 01/01/2026 0030");
         } catch (JohnDoeException e) {
-            String errorMessage = "  Please provide a task name.\n"
-                    + "  Enter 'deadline' for more help.\n\n> ";
+            String errorMessage =
+                    "  Please provide a task name.\n" + HELP_SUFFIX;
+            assertEquals(errorMessage, e.getMessage());
+        }
+    }
+
+    @Test
+    public void constructDeadline_noDeadline_exceptionThrown() {
+        try {
+            new DeadlineCommand("sleep /by");
+        } catch (JohnDoeException e) {
+            String errorMessage =
+                    "  Please provide a deadline.\n" + HELP_SUFFIX;
+            assertEquals(errorMessage, e.getMessage());
+        }
+    }
+
+    @Test
+    public void constructDeadline_invalidDateTimeFormat_exceptionThrown() {
+        try {
+            new DeadlineCommand("sleep /by 01-01-2026 00:30");
+        } catch (JohnDoeException e) {
+            String errorMessage =
+                    "  Invalid date & time format.\n" + HELP_SUFFIX;
             assertEquals(errorMessage, e.getMessage());
         }
     }
--- src/test/java/johndoe/command/EventCommandTest.java
+++ src/test/java/johndoe/command/EventCommandTest.java
@@ -7,13 +7,81 @@ import org.junit.jupiter.api.Test;
 import johndoe.exception.JohnDoeException;
 
 public class EventCommandTest {
+    private static final String HELP_SUFFIX = "  Enter 'event' for more help.\n\n";
+
+    @Test
+    public void constructEvent_keywordsAreSubstring_exceptionThrown() {
+        try {
+            new EventCommand("meeting/from 01/01/2026 0000 /to 01/01/2026 0100");
+        } catch (JohnDoeException e) {
+            String errorMessage =
+                    "  There should be spaces before and after '/from' and '/to'.\n" + HELP_SUFFIX;
+            assertEquals(errorMessage, e.getMessage());
+        }
+    }
+
+    @Test
+    public void constructEvent_keywordsRepeated_exceptionThrown() {
+        try {
+            new EventCommand("meeting /from /from 01/01/2026 0000 /to 01/01/2026 0100");
+        } catch (JohnDoeException e) {
+            String errorMessage =
+                    "  Exactly one '/from' and one '/to' must be used.\n" + HELP_SUFFIX;
+            assertEquals(errorMessage, e.getMessage());
+        }
+    }
+
     @Test
     public void constructEvent_swapFromTo_exceptionThrown() {
         try {
-            new EventCommand("lecture /to 1pm /from 2pm");
+            new EventCommand("meeting /to 01/01/2026 0100 /from 01/01/2026 0000");
         } catch (JohnDoeException e) {
-            String errorMessage = "  '/from' must be before '/to'.\n"
-                    + "  Enter 'event' for more help.\n\n> ";
+            String errorMessage =
+                    "  '/from' must be before '/to'.\n" + HELP_SUFFIX;
+            assertEquals(errorMessage, e.getMessage());
+        }
+    }
+
+    @Test
+    public void constructEvent_noName_exceptionThrown() {
+        try {
+            new EventCommand("/from 01/01/2026 0000 /to 01/01/2026 0100");
+        } catch (JohnDoeException e) {
+            String errorMessage =
+                    "  Please provide a task name.\n" + HELP_SUFFIX;
+            assertEquals(errorMessage, e.getMessage());
+        }
+    }
+
+    @Test
+    public void constructEvent_noStart_exceptionThrown() {
+        try {
+            new EventCommand("meeting /from /to 01/01/2026 0100");
+        } catch (JohnDoeException e) {
+            String errorMessage =
+                    "  Please provide a start time.\n" + HELP_SUFFIX;
+            assertEquals(errorMessage, e.getMessage());
+        }
+    }
+
+    @Test
+    public void constructEvent_noEnd_exceptionThrown() {
+        try {
+            new EventCommand("meeting /from 01/01/2026 0000 /to");
+        } catch (JohnDoeException e) {
+            String errorMessage =
+                    "  Please provide an end time.\n" + HELP_SUFFIX;
+            assertEquals(errorMessage, e.getMessage());
+        }
+    }
+
+    @Test
+    public void constructEvent_invalidDateTimeFormat_exceptionThrown() {
+        try {
+            new EventCommand("meeting /from 01-01-2026 00:00 /to 01/01/2026 0100");
+        } catch (JohnDoeException e) {
+            String errorMessage =
+                    "  Invalid date & time format.\n" + HELP_SUFFIX;
             assertEquals(errorMessage, e.getMessage());
         }
     }
